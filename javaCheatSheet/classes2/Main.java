d750d69bb5c805d01c4641761431eae3f536df5d
Wenke Huang <whuang67@illinois.edu>
2018-01-03 18:04:29 -0600

    Add files via upload

---
+++ javaCheatSheet/classes2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(34);
+        outlander.accelerate(16);
+        outlander.accelerate(-40);
+        outlander.accelerate(-10);
+    }
+}
---
+++ javaCheatSheet/classes2/Vehicle.java
@@ -0,0 +1,40 @@
+public class Vehicle {
+    private String name;
+    private String size;
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
+
+    public void steer(int direction) {
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at "+this.currentDirection+" degrees.");
+    }
+    public void move(int velocity, int direction) {
+        this.currentVelocity = velocity;
+        this.currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at "+this.currentVelocity+" in direction "+this.currentDirection);
+    }
+    public void stop() {
+        this.currentVelocity = 0;
+        System.out.println("Vehicle.stop(): Vehicle stops.");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSize() {
+        return size;
+    }
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+}
