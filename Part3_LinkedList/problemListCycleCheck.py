21c77996f2c1b47eadd7a1f0f93fd6ad084afe28
Wenke Huang <whuang67@illinois.edu>
2018-01-07 19:57:21 -0600

    Update problemListCycleCheck.py

--- Part3_LinkedList/problemListCycleCheck.py
+++ Part3_LinkedList/problemListCycleCheck.py
@@ -22,14 +22,21 @@ class Node(object):
         self.nextnode = None
 
 def cycle_check(node):
-    lst = [node.value]
-    current_node = node
-    while current_node.nextnode:
-        val = current_node.nextnode.value
-        if val in lst:
+
+    marker1 = node
+    marker2 = node
+
+    while marker2 != None and marker2.nextnode != None:
+        
+        # Note
+        marker1 = marker1.nextnode
+        marker2 = marker2.nextnode.nextnode
+
+        # Check if the markers have matched
+        if marker2 == marker1:
             return True
-        lst.append(current_node.nextnode.value)
-        current_node = current_node.nextnode
+
+    # Case where marker ahead reaches the end of the list
     return False
 
 if __name__ == "__main__":
