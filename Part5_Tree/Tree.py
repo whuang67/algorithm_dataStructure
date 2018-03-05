7e36148812bd2850d6841c8083bbfdb706c8cbb2
Wenke Huang <huangwk223@gmail.com>
2018-03-05 11:31:39 -0800

    Update Tree.py

--- Part5_Tree/Tree.py
+++ Part5_Tree/Tree.py
@@ -6,34 +6,21 @@ Created on Mon Jan 01 23:48:29 2018
 """
 
 ###### Binary Tree 1 ######
-def binaryTree(root):
-    return [root, [], []]
 
+def BinaryTree(r):
+    return [r, [], []]
 def insertLeft(root, newBranch):
-    t = root.pop(1)
-    if len(t) > 1:
-        root.insert(1, [newBranch, t, []])
-    else:
-        root.insert(1, [newBranch, [], []])
+    root.insert(1, [newBranch, root.pop(1), []])
     return root
-
 def insertRight(root, newBranch):
-    t = root.pop(2)
-    if len(t) > 1:
-        root.insert(2, [newBranch, [], t])
-    else:
-        root.insert(2, [newBranch, [], []])
+    root.insert(2, [newBranch, [], root.pop(2)])
     return root
-
 def getRootValue(root):
     return root[0]
-
-def setRootValue(root, newValue):
-    root[0] = newValue
-
+def setRootValue(root, new_value):
+    root[0] = new_value
 def getLeftChild(root):
     return root[1]
-
 def getRightChild(root):
     return root[2]
 
